4dc1930b394ba9ed1a91236c03dacbb178f69988
Vladimir Bazhanov <vladimir.a.bazhanov@gmail.com>
2023-09-07 15:19:38 +0300

    Added django-extensions; Added base template layout; Added users/index page

---
+++ task_manager/users/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.contrib.auth.models import User
+
+def index(request):
+    users = User.objects.all()
+    context = { 'users': users }
+
+    return render(request, 'users/index.html', context)
