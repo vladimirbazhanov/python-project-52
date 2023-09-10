ba1bfdb38e35a60b3ff8c20b56f402f762438f6f
Vladimir Bazhanov <vladimir.a.bazhanov@gmail.com>
2023-09-10 18:02:26 +0300

    some styles

--- task_manager/tasks/forms.py
+++ task_manager/tasks/forms.py
@@ -10,7 +10,7 @@ class TaskForm(forms.ModelForm):
     description = forms.CharField(label='Описание', max_length=128, widget=forms.TextInput(attrs={'class': 'form-control'}))
     executor = forms.ModelChoiceField(User.objects.all(), widget=forms.Select(attrs={'class': 'form-select'}), label='Исполнитель')
     status = forms.ModelChoiceField(Status.objects.all(), widget=forms.Select(attrs={'class': 'form-select'}), label='Статус')
-    labels = forms.ModelMultipleChoiceField(Label.objects.all())
+    labels = forms.ModelMultipleChoiceField(Label.objects.all(), widget=forms.SelectMultiple(attrs={'class': 'form-select'}), label='Статус')
 
     class Meta:
         model = Task
