24ade63b32dd4a0aa8619415a670df50895aa782
Vladimir Bazhanov <vladimir.a.bazhanov@gmail.com>
2023-09-10 15:54:42 +0300

    registering process

--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
 from django.views import View
 from django.contrib.auth import get_user_model
-from task_manager.users.models import User
 from task_manager.users.forms import UserForm
+from django.contrib import auth
 
 
 def index(request):
@@ -18,3 +18,15 @@ class CreateUserView(View):
     def get(self, request, *args, **kwargs):
         form = UserForm()
         return render(request, 'users/create.html', {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = UserForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = auth.authenticate(username=username, password=password)
+            auth.login(request, user)
+            return HttpResponseRedirect('/')
+        else:
+            return render(request, 'users/create.html', {'form': form})
