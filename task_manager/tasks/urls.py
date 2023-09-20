45ecb26513d05451bd27a26ef8d6ff27ce37768c
Vladimir Bazhanov <vladimir.a.bazhanov@gmail.com>
2023-09-20 16:01:02 +0300

    url reorder

--- task_manager/tasks/urls.py
+++ task_manager/tasks/urls.py
@@ -5,8 +5,8 @@ app_name = 'tasks'
 
 urlpatterns = [
     path('', views.TasksView.as_view(), name='index'),
-    path('<id>/', views.TaskView.as_view(), name='show'),
     path('create/', views.CreateTaskView.as_view(), name='create'),
+    path('<id>/', views.TaskView.as_view(), name='show'),
     path('<id>/update/', views.UpdateTaskView.as_view(), name='update'),
     path('<id>/delete/', views.DeleteTaskView.as_view(), name='delete')
 ]
