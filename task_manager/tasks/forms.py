c459737c5187db8a9a001b0b7dd0e7c6d91e7de4
Vladimir Bazhanov <vladimir.a.bazhanov@gmail.com>
2023-09-20 16:15:55 +0300

    labels not required for task

--- task_manager/tasks/forms.py
+++ task_manager/tasks/forms.py
@@ -10,7 +10,7 @@ class TaskForm(forms.ModelForm):
     description = forms.CharField(label='Описание', max_length=128, widget=forms.TextInput(attrs={'class': 'form-control'}))
     executor = forms.ModelChoiceField(User.objects.all(), widget=forms.Select(attrs={'class': 'form-select'}), label='Исполнитель')
     status = forms.ModelChoiceField(Status.objects.all(), widget=forms.Select(attrs={'class': 'form-select'}), label='Статус')
-    labels = forms.ModelMultipleChoiceField(Label.objects.all(), widget=forms.SelectMultiple(attrs={'class': 'form-select'}), label='Метки')
+    labels = forms.ModelMultipleChoiceField(Label.objects.all(), required=False, widget=forms.SelectMultiple(attrs={'class': 'form-select'}), label='Метки')
 
     class Meta:
         model = Task
