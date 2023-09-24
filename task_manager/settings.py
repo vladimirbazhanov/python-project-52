4b5bb19d320d0bd6c00d155cf400c107b71e7ce7
Vladimir Bazhanov <vladimir.a.bazhanov@gmail.com>
2023-09-24 13:24:21 +0300

    database using env veriable

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
+import dj_database_url
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +28,7 @@ AUTH_USER_MODEL = 'users.User'
 SECRET_KEY = 'django-insecure-uqiu7&tx%1ndm(7v247=-tn!zqmf_vvqmqd%k=^ai12m%s#1fy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ['DEBUG']
 
 ALLOWED_HOSTS = ['127.0.0.1', 'taskmanager-pe0c.onrender.com', 'webserver']
 
@@ -82,11 +86,9 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ['DATABASE_URL'])
 }
 
 
