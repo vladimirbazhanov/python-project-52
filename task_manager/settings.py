0b5931903d7df199b0a42fa624cf4244705acf00
Vladimir Bazhanov <vladimir.a.bazhanov@gmail.com>
2023-09-24 14:20:31 +0300

    rollbar init

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -135,7 +135,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = '/login'
 
-if os.environ['DJANGO_ENV'] == 'production':
+if os.environ.get('DJANGO_ENV') == 'production':
     ROLLBAR = {
         'access_token': 'c32f11038ee447669fe1b213f5f9f461',
         'environment': 'production',
